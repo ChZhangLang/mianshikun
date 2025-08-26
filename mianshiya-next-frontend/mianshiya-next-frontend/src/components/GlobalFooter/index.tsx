1706441706a0f0d74fab4f6df41eada1dec21f15
“1626216058@qq.com” <1626216058@qq.com>
2025-08-26 15:17:58 +0800

    Initial commit

---
+++ mianshiya-next-frontend/mianshiya-next-frontend/src/components/GlobalFooter/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.css";
+
+/**
+ * 全局底部栏组件
+ * @constructor
+ */
+export default function GlobalFooter() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="global-footer">
+      <div>© {currentYear} 面试刷题战场</div>
+      <div>
+        <a  target="_blank">
+          作者：程序员梓豪
+        </a>
+      </div>
+    </div>
+  );
+}
